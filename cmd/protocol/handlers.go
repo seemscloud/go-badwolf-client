123af20798e6ea0d8b3ea4c456a401becba9e3bf
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-05-04 00:56:06 +0200

    asdf

--- cmd/protocol/handlers.go
+++ cmd/protocol/handlers.go
@@ -1,9 +1,13 @@
 package protocol
 
+import "time"
+
 func PingHandler() {
 	conn := connCreate()
 	for {
 		messageBytes := PingPongBuilder()
 		connWrite(&conn, messageBytes)
+
+		time.Sleep(time.Second)
 	}
 }
